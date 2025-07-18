65668e465b5324696c5e0b0dc9208cc1fe309016
Ahmed Elmogy <ahmed.elmogy2005@gmail.com>
2025-07-18 19:27:34 +0300

    endDesignSiteBar

--- src/app/components/side-bar/side-bar.ts
+++ src/app/components/side-bar/side-bar.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MessageModule } from 'primeng/message';
 
 @Component({
   selector: 'app-side-bar',
-  imports: [MessageModule],
+  imports: [MessageModule,CommonModule],
   templateUrl: './side-bar.html',
   styleUrl: './side-bar.css'
 })
 export class SideBar {
 
+  stateSideBar:boolean=true;
+  changeState():void{
+   if(this.stateSideBar)
+     this.stateSideBar=false;
+    else
+       this.stateSideBar=true;
+  }
 }
